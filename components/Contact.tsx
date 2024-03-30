3383ddcf96be1ed9091634babc824c7336e62efd
CZEJAY <calebjimmy67@gmail.com>
2024-03-30 02:44:35 +0100

    btn

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -78,7 +78,7 @@ export default function Contact() {
           required
           maxLength={5000}
         />
-        <Button className="text-gray-950 dark:text-gray-50" type="submit">Submit</Button>
+        <Button className="relative inline-flex h-fit w-fit cursor-pointer rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2" type="submit">Submit</Button>
       </form>
     </motion.section>
   );
