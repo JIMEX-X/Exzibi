da0162e62524d39a51b08423220c81363c9afa8c
CZEJAY <calebjimmy67@gmail.com>
2024-04-01 16:09:30 +0100

    email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,18 +2,19 @@
 
 import React from "react";
 import { validateString, getErrorMessage } from "@/lib/utils";
-import ContactFormEmail from "@/email/contact-form-email";
-import {transporter} from "@/lib/nodemailer.config"
+import { transporter } from "@/lib/nodemailer.config";
+import { MailOptions } from "nodemailer/lib/sendmail-transport";
+
 
 export const sendEmail = async (formData: FormData) => {
-  if(!process.env.NODEMAILER_HOST && !process.env.NODEMAILER_PORT){
+  if (!process.env.NODEMAILER_HOST && !process.env.NODEMAILER_PORT) {
     return {
-      error: "Email service is not configured"
-    }
+      error: "Email service is not configured",
+    };
   }
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
-
+  const context = {message, senderEmail}
   // simple server-side validation
   if (!validateString(senderEmail, 500)) {
     return {
@@ -28,25 +29,40 @@ export const sendEmail = async (formData: FormData) => {
 
   let data;
   try {
-    const mailoptions = {
-      from: "Contact Form <onboarding@resend.dev>",
+    const mailoptions: MailOptions = {
+      from: senderEmail,
       to: "abuopearl@gmail.com",
       subject: "Message from portfolio",
-      reply_to: senderEmail,
-      react: React.createElement(ContactFormEmail, {
-        message: message,
-        senderEmail: senderEmail,
-      }),
-    }
+      html: `<html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>New message from your portfolio site</title>
+      </head>
+      <body style="height: 100vh; justify-content: center; display: flex; align-items: center; background-color: white; color: black;" className="bg-gray-100 text-black">
+        <div style="width: fit-content; height: fit-content; border: 2px solid; border-radius: .5rem; padding: 30px; display: flex; align-items: center;">
+          <section className="">
+            <h1 style="line-height: 1.25; font-size: 0.875rem; line-height: 1.25rem" className="leading-tight text-sm">
+              You received the following message from the contact form
+            </h1>
+            <p>${message}</p>
+            <hr />
+            <p>The sender's email is: ${senderEmail}</p>
+          </section>
+        </div>
+      </body>
+    </html>`,
+    };
     data = await transporter.sendMail(mailoptions, (err, info) => {
-      if(err){
+      if (err) {
         return {
-          error: getErrorMessage(err)
-        }
-      }else{
-        console.log(`Email Sent: ${info.response}`)
+          error: getErrorMessage(err),
+        };
+      } else {
+        console.log(`Email Sent: ${info.response}`);
       }
-    })
+    });
   } catch (error: unknown) {
     return {
       error: getErrorMessage(error),
--- lib/nodemailer.config.ts
+++ lib/nodemailer.config.ts
@@ -3,6 +3,7 @@ export const transporter = nodemailer.createTransport({
     service: "gmail",
     port: 465,
     host: process.env.NODEMAILER_HOST,
+    secure: true,
     auth: {
         pass: process.env.NODEMAILER_PASS,
         user: process.env.NODEMAILER_USER
